789ea6fa998c71afe2c9a8f522a781ada047ab85
KUMARANKIT3012 <ankitkumar30122004@gmail.com>
2026-04-24 14:19:39 +0530

    Update transaction form behavior

--- features/transactions/components/transaction-form.tsx
+++ features/transactions/components/transaction-form.tsx
@@ -21,7 +21,6 @@ import { Select } from "@/components/select";
 import { Textarea } from "@/components/ui/textarea";
 import { convertAmountToMiliunits } from "@/lib/utils";
 
-
 const formSchema = z.object({
   date: z.coerce.date(),
   accountId: z.string(),
@@ -35,10 +34,8 @@ const apiSchema = insertTransactionSchema.omit({
   id: true,
 });
 
-type FormValues = z.input<typeof formSchema>;
+type FormValues = z.infer<typeof formSchema>;
 type ApiFormValues = z.input<typeof apiSchema>;
-
-
 
 type Props = {
   id?: string;
@@ -46,10 +43,10 @@ type Props = {
   onSubmit: (values: ApiFormValues) => void;
   onDelete?: () => void;
   disabled?: boolean;
-  accountOptions: { label: string; value: string; }[];
-    categoryOptions: { label: string; value: string; }[];
-    onCreateAccount: (name: string) => void;
-    onCreateCategory: (name: string) => void;
+  accountOptions: { label: string; value: string }[];
+  categoryOptions: { label: string; value: string }[];
+  onCreateAccount: (name: string) => void;
+  onCreateCategory: (name: string) => void;
 };
 
 export const TransactionForm = ({
@@ -58,165 +55,153 @@ export const TransactionForm = ({
   onSubmit,
   onDelete,
   disabled,
-    accountOptions,
-    categoryOptions,
-    onCreateAccount,
-    onCreateCategory,
-}: Props) =>{
-    const form = useForm<FormValues>({
+  accountOptions,
+  categoryOptions,
+  onCreateAccount,
+  onCreateCategory,
+}: Props) => {
+  const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: defaultValues,
+  });
+
+  const handleSubmit = (values: FormValues) => {
+    const amount = parseFloat(values.amount);
+    const amountInMiliunits = convertAmountToMiliunits(amount);
+    onSubmit({
+      ...values,
+      amount: amountInMiliunits,
     });
+  };
 
-    const handleSubmit = (values: FormValues) => {
-        const amount = parseFloat(values.amount);
-        const amountInMiliunits = convertAmountToMiliunits(amount);
-        onSubmit({
-        ...values,
-        amount: amountInMiliunits,
-        });
-    };
-
-    const handleDelete = () => {
+  const handleDelete = () => {
     onDelete?.();
-    };
+  };
 
-    return (
-        <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(handleSubmit)}
-              className="space-y-4 pt-4"
-            >
-                <FormField 
-                    name="date"
-                    control={form.control}
-                    render={({field}) => (
-                        <FormItem>
-                            <FormControl>
-                                <DatePicker 
-                                    value={field.value}
-                                    onChange={field.onChange}
-                                    disabled={disabled}
-                                />
-                            </FormControl>
-                        </FormItem>
-                    )}    
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(handleSubmit)}
+        className="space-y-4 pt-4"
+      >
+        <FormField
+          name="date"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <DatePicker
+                  value={field.value}
+                  onChange={field.onChange}
+                  disabled={disabled}
                 />
-                <FormField 
-                    name="accountId"
-                    control={form.control}
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>
-                                Account
-                            </FormLabel>
-                            <FormControl>
-                                <Select
-                                placeholder="Select an account"
-                                options={accountOptions}
-                                onCreate={onCreateAccount}
-                                value={field.value}
-                                onChange={field.onChange}
-                                disabled={disabled}
-                                />
-                            </FormControl>
-                        </FormItem>
-                    )}
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="accountId"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Account</FormLabel>
+              <FormControl>
+                <Select
+                  placeholder="Select an account"
+                  options={accountOptions}
+                  onCreate={onCreateAccount}
+                  value={field.value}
+                  onChange={field.onChange}
+                  disabled={disabled}
                 />
-                <FormField 
-                    name="categoryId"
-                    control={form.control}
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>
-                                Category
-                            </FormLabel>
-                            <FormControl>
-                                <Select
-                                placeholder="Select an category"
-                                options={categoryOptions}
-                                onCreate={onCreateCategory}
-                                value={field.value}
-                                onChange={field.onChange}
-                                disabled={disabled}
-                                />
-                            </FormControl>
-                        </FormItem>
-                    )}
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="categoryId"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Category</FormLabel>
+              <FormControl>
+                <Select
+                  placeholder="Select an category"
+                  options={categoryOptions}
+                  onCreate={onCreateCategory}
+                  value={field.value}
+                  onChange={field.onChange}
+                  disabled={disabled}
                 />
-                <FormField 
-                    name="payee"
-                    control={form.control}
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>
-                                Payee
-                            </FormLabel>
-                            <FormControl>
-                                <Input 
-                                    disabled={disabled}
-                                    placeholder="Add a payee"
-                                    {...field}
-                                />
-                            </FormControl>
-                        </FormItem>
-                    )}
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="payee"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Payee</FormLabel>
+              <FormControl>
+                <Input
+                  disabled={disabled}
+                  placeholder="Add a payee"
+                  {...field}
                 />
-                <FormField 
-                    name="amount"
-                    control={form.control}
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>
-                                Amount
-                            </FormLabel>
-                            <FormControl>
-                                <AmountInput 
-                                    {...field}
-                                    disabled={disabled}
-                                    placeholder="0.00"
-                                />
-                            </FormControl>
-                        </FormItem>
-                    )}
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="amount"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Amount</FormLabel>
+              <FormControl>
+                <AmountInput
+                  {...field}
+                  disabled={disabled}
+                  placeholder="0.00"
                 />
-                <FormField 
-                    name="notes"
-                    control={form.control}
-                    render={({field}) => (
-                        <FormItem>
-                            <FormLabel>
-                                Notes
-                            </FormLabel>
-                            <FormControl>
-                                <Textarea
-                                    {...field}
-                                    value={field.value ?? ""}
-                                    disabled={disabled}
-                                    placeholder="Optional notes"
-                                />
-                            </FormControl>
-                        </FormItem>
-                    )}
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          name="notes"
+          control={form.control}
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Notes</FormLabel>
+              <FormControl>
+                <Textarea
+                  {...field}
+                  value={field.value ?? ""}
+                  disabled={disabled}
+                  placeholder="Optional notes"
                 />
-                <Button className="w-full" disabled={disabled}>
-                {id ? "Save changes" : "Create transaction"}
-                </Button>
-
-                {!!id &&(
-                    <Button
-                    type="button"
-                    disabled={disabled}
-                    onClick={handleDelete}
-                    className="w-full"
-                    variant="outline"
-                    >
-                    <Trash className="size-4 mr-2" />
-                        Delete transaction
-                    </Button>
-                )}
-            </form>
-
-        </Form>
-    )
-}
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <Button className="w-full" disabled={disabled}>
+          {id ? "Save changes" : "Create transaction"}
+        </Button>
+        {!!id && (
+          <Button
+            type="button"
+            disabled={disabled}
+            onClick={handleDelete}
+            className="w-full"
+            variant="outline"
+          >
+            <Trash className="size-4 mr-2" />
+            Delete transaction
+          </Button>
+        )}
+      </form>
+    </Form>
+  );
+};
